21a8cfc678a78c1349c4559a33562e14c872f03b
Administrator <744890753@qq.com>
2025-10-14 22:25:52 +0800

    修改【页面顶部组件】用户信息展示布局

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import styles from "./page.module.css";
 import "../styles/common.css"
 import "../styles/flex.css"
-import BlogHead from "../components/blog-head/blog-head";
+import BlogHeadWrap from "../components/blog-head/blog-head-wrap";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <BlogHead/>
+      <BlogHeadWrap/>
     </div>
   );
 }
--- src/components/blog-head/blog-head.tsx
+++ src/components/blog-head/blog-head.tsx
@@ -5,8 +5,8 @@ import PropTypes from "prop-types"
 import {UserInfo} from "../../types/user";
 
 
-export default function BlogHead({userInfo}: {userInfo:UserInfo}) {
-
+function BlogHead({ userInfo }: {userInfo:UserInfo}) {
+    console.log("userInfo=", userInfo)
 
     /**
      * 退出登录
@@ -18,20 +18,31 @@ export default function BlogHead({userInfo}: {userInfo:UserInfo}) {
      * 登录
      */
     const login = () => {
-
+        console.log("userInfo=", userInfo)
     }
+
 
   return <div className={"blog-head-container row-between"}>
       <input placeholder="Search" className={"blog-head-input"}/>
-      <div className={"user-info-container"}>
+      <div className={"head-right-container"}>
           {
-              userInfo ? <div>
+              userInfo ? <div className={"user-info-container"}>
                   <div className={"user-head-container"}>
-                      <img class={"user-head-img"} src={userInfo.headImg ||  ""} alt=""/>
-                      <button class={"logout-btn"} onClick={logOut}>退出登录</button>
+                      <img className={"user-head-img"} src={userInfo.headImg ||  ""} alt=""/>
                   </div>
+                  <div className={"user-drap-down-container"}>
+                      <ul className={"user-info-list"}>
+                          <li className={"user-info-item"}>{userInfo.name}</li>
+                          <li className={"user-info-item"}>{userInfo.email}</li>
+                          <li className={"user-info-item"}>{userInfo.phone}</li>
+                      </ul>
+                      <div className={"user-logout-container"} onClick={logOut}>
+                          退出登录
+                      </div>
+                  </div>
+
               </div> : <div>
-                  <button class={"login-btn"} onClick={login}>登录</button>
+                  <button className={"login-btn"} onClick={login}>登录</button>
               </div>
           }
       </div>
@@ -39,5 +50,7 @@ export default function BlogHead({userInfo}: {userInfo:UserInfo}) {
 }
 
 BlogHead.prototype = {
-    userInfo: PropTypes.object.isRequiredv
+    userInfo: PropTypes.object.isRequired
 }
+
+export default BlogHead
