6021274cb9391ce7b7d3093045c8469aad283972
Administrator <744890753@qq.com>
2025-10-15 20:59:40 +0800

    修改【页面顶部】添加创作按钮

--- src/components/blog-head/blog-head.tsx
+++ src/components/blog-head/blog-head.tsx
@@ -9,6 +9,7 @@ import IconUser from "../icons/icon-user";
 import IconPhone from "../icons/icon-phone";
 import IconLogout from "../icons/icon-logout";
 import IconTrangle from "../icons/icon-trangle";
+import IconPlus from "../icons/icon-plus";
 
 
 function BlogHead({ userInfo }: {userInfo:UserInfo}) {
@@ -38,35 +39,41 @@ function BlogHead({ userInfo }: {userInfo:UserInfo}) {
           </div>
           <div className={"head-right-container"}>
               {
-                  userInfo ? <div className="user-info-container">
-                      <div className="user-head-container cursor-pointer">
-                          <img className={"user-head-img"} src={userInfo.headImg ||  ""} alt=""/>
-                      </div>
-                      <div className="user-drap-down-container absolute rounded-md">
-                          <ul className={"user-info-list py-2"}>
-                              <li className="user-info-item flex items-center">
-                                  <IconUser width={20} height={20}/>
-                                  <p className="pl-5 text-xl">{userInfo.name}</p>
-                              </li>
-                              <li className="user-info-item flex items-center">
-                                  <IconEmail width={20} height={20}/>
-                                  <p className="pl-5 text-xl">{userInfo.email}</p>
-                              </li>
-                              <li className="user-info-item flex items-center">
-                                  <IconPhone width={20} height={20}/>
-                                  <p className="pl-5 text-xl">{userInfo.phone}</p>
-                              </li>
-                          </ul>
-                          <div className="user-logout-container cursor-pointer flex items-center" onClick={logOut}>
-                              <IconLogout width={20} height={20}/>
-                              <p className="pl-5 text-xl">退出登录</p>
+                  userInfo ? <div className="flex justify-end items-center">
+                      <div className="user-info-container relative">
+                          <div className="user-head-container cursor-pointer">
+                              <img className={"user-head-img"} src={userInfo.headImg ||  ""} alt=""/>
                           </div>
-                          <span  className="icon-trangle">
+                          <div className="user-drap-down-container absolute rounded-md">
+                              <ul className={"user-info-list py-2 px-4"}>
+                                  <li className="user-info-item flex items-center">
+                                      <IconUser width={20} height={20}/>
+                                      <p className="pl-1 text-xl">{userInfo.name}</p>
+                                  </li>
+                                  <li className="user-info-item flex items-center">
+                                      <IconEmail width={20} height={20}/>
+                                      <p className="pl-1 text-xl">{userInfo.email}</p>
+                                  </li>
+                                  <li className="user-info-item flex items-center">
+                                      <IconPhone width={20} height={20}/>
+                                      <p className="pl-1 text-xl">{userInfo.phone}</p>
+                                  </li>
+                              </ul>
+                              <div className="user-logout-container cursor-pointer flex items-center" onClick={logOut}>
+                                  <IconLogout width={20} height={20}/>
+                                  <p className="pl-1 text-xl">退出登录</p>
+                              </div>
+                              <span  className="icon-trangle">
                               <IconTrangle width={30} height={30} color="#ffffff"/>
                           </span>
-                      </div>
+                          </div>
 
-                  </div> : <div>
+                      </div>
+                      <div className="create-container ml-10 flex items-center py-2 px-6 rounded-full cursor-pointer">
+                          <IconPlus width={20} height={20} color="#ffffff"/>
+                          <span className="text-xl text-white ml-1">创作</span>
+                      </div>
+                  </div>: <div>
                       <button className={"login-btn"} onClick={login}>登录</button>
                   </div>
               }
---
+++ src/components/icons/icon-plus.tsx
@@ -0,0 +1,7 @@
+
+export default function IconPlus({ width = 30, height = 30, color = "#272636" } : { width?: number, height?: number, color?: string}){
+
+    return <svg t="1760532638144" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11281" width={width} height={height}>
+        <path d="M469.333333 469.333333V170.666667h85.333334v298.666666h298.666666v85.333334h-298.666666v298.666666h-85.333334v-298.666666H170.666667v-85.333334h298.666666z" p-id="11282"  fill={color}/>
+    </svg>
+}
